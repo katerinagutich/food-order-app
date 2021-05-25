b537bf0b7bedeeced660e5022cdf0d0049003092
Kate <katerina_gutich@mail.ru>
2021-05-25 20:53:04 +0300

    Add MealItem to AvailableMeals and Meals to App component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import styles from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 import Card from '../UI/Card';
 
 const DUMMY_MEALS = [
@@ -30,7 +31,13 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
 
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+    const mealsList = DUMMY_MEALS.map(meal =>
+        <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />)
 
     return (
         <section className={styles.meals}>
