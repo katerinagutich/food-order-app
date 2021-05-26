5255bdc303dd1dbf01bfac404d499d2792993bc3
Kate <katerina_gutich@mail.ru>
2021-05-26 12:56:23 +0300

    Add useReducer to manage cart state when a new item is added to the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContex from '../../store/cart-context';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+
+    const ctx = useContext(CartContex);
+
+    const cartItemsNumber = ctx.items.reduce((curNumber, item) => {
+       return curNumber += item.amount;
+    }, 0);
+
     return (
         <button onClick={props.onClick} className={styles.button}>
             <span className={styles.icon}>
                 <CartIcon/>
             </span>
             <span>Your Cart</span>
-            <span className={styles.badge}>
-                3
-            </span>
+            <span className={styles.badge}>{cartItemsNumber}</span>
         </button>
     );
 };
