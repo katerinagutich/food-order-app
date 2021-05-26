fe3cb7a923e7dff220eb4a76db1b84f8373f9bc9
Kate <katerina_gutich@mail.ru>
2021-05-26 18:05:55 +0300

    Refactor cartItemAddHandler in Cart component, set amount to 1

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -15,8 +15,8 @@ const Cart = props => {
         cartCtx.removeItem(id);
     };
 
-    const cartItemAddHandler = item => {
-        cartCtx.addItem(item);
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({ ...item, amount: 1 });
     };
 
     const cartItems = (
